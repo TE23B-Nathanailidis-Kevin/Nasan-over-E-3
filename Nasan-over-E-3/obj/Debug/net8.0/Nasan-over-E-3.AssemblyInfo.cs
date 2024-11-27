5a7c394ae18b6ed5f8dcd5d6b4f6d799f91b4f1c
Kevin Nathanailidis <kevin.nathanailidis@elev.ga.ntig.se>
2024-11-27 15:07:11 +0100

    km

--- Nasan-over-E-3/obj/Debug/net8.0/Nasan-over-E-3.AssemblyInfo.cs
+++ Nasan-over-E-3/obj/Debug/net8.0/Nasan-over-E-3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Nasan-over-E-3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24ade26a7bace26785e04513c42561d58ec524ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88ad1061495d118ef2775de02e6453e476f7c5e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Nasan-over-E-3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Nasan-over-E-3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
